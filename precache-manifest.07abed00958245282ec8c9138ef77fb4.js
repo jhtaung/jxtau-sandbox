8f12853364b2b446459dd287edb91b28e625eb6f
Justin Htaung <jhtaung29@gmail.com>
2020-01-11 12:46:13 -0800

    Updates

---
+++ precache-manifest.07abed00958245282ec8c9138ef77fb4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f3f18e3e2b704137f84897b47c0722",
+    "url": "/jxtau-sandbox/index.html"
+  },
+  {
+    "revision": "fe97dd454d37e92a9a86",
+    "url": "/jxtau-sandbox/static/js/2.db7d580e.chunk.js"
+  },
+  {
+    "revision": "f7344d93d05a18c65def",
+    "url": "/jxtau-sandbox/static/js/main.14605bbe.chunk.js"
+  },
+  {
+    "revision": "64ce262d09f2ffaa6779",
+    "url": "/jxtau-sandbox/static/js/runtime-main.a6cd8d04.js"
+  }
+]);
