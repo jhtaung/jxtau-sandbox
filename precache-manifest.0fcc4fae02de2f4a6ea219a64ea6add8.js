cdfce95e4ea9a2cc17d7b226ebaa8ff112073f1b
Justin Htaung <jhtaung29@gmail.com>
2020-01-11 13:00:21 -0800

    Updates

--- precache-manifest.0fcc4fae02de2f4a6ea219a64ea6add8.js
+++ precache-manifest.0fcc4fae02de2f4a6ea219a64ea6add8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b492bf0c1d7aeba79a16beeff488bdb",
+    "revision": "604e142205063e94aafce3597979ef3e",
     "url": "/jxtau-sandbox/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/jxtau-sandbox/static/js/2.db7d580e.chunk.js"
   },
   {
-    "revision": "5b3a5df1c3b5a45426f9",
-    "url": "/jxtau-sandbox/static/js/main.6d740c20.chunk.js"
+    "revision": "6510dbe181baa801d4b3",
+    "url": "/jxtau-sandbox/static/js/main.a86e73cd.chunk.js"
   },
   {
     "revision": "64ce262d09f2ffaa6779",
